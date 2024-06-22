bceba80d3e0130d80dd51dc7f48dbdc7923983b0
ThisIsQasim <18313886+ThisIsQasim@users.noreply.github.com>
2024-06-22 19:54:58 +0500

    Add renovate bot

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "endpoint": "https://git.project.com/api/v1",
+    "gitAuthor": "Renovate<renovate@project.com>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json",
+    "autodiscover": true,
+    "optimizeForDisabled": true,
+};
